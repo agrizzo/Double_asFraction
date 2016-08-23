7501bf955597021737e4126a183436e54166fa65
Tony Rizzo <trizzo@koteray.com>
2016-08-23 15:49:53 -0400

    Initial Commit

---
+++ Double_asFraction/Double_asFraction.h
@@ -0,0 +1,19 @@
+//
+//  Double_asFraction.h
+//  Double_asFraction
+//
+//  Created by Tony Rizzo on 8/23/16.
+//  Copyright © 2016 Koteray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Double_asFraction.
+FOUNDATION_EXPORT double Double_asFractionVersionNumber;
+
+//! Project version string for Double_asFraction.
+FOUNDATION_EXPORT const unsigned char Double_asFractionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Double_asFraction/PublicHeader.h>
+
+
